215f95647c2bae7ed5f539229e3195d8452a9604
ashkan <khodaei542@gmail.com>
2025-07-02 12:01:11 +0330

    update forgot password

--- forgot_password.cpp
+++ forgot_password.cpp
@@ -10,49 +10,11 @@ forgot_password::forgot_password(QWidget *parent)
     , ui(new Ui::forgot_password)
 {
     ui->setupUi(this);
-
-    back_button = new QPushButton();
-
-    connect(this,&forgot_password::click_back_button,this,&forgot_password::send_message);
-
-    connect(this,&forgot_password::receive_message,this,&forgot_password::on_back_button_2_clicked);
-
-    connect(this,&forgot_password::click_forgot_password_button,this,&forgot_password::send_message);
-
-    connect(this,&forgot_password::receive_message,this,&forgot_password::on_forgot_password_button_clicked);
-
-    socket.connectToHost("127.0.0.1",6006);
-
-    if(socket.waitForConnected(1000))
-    {
-
-        receive_message();
-
-    }
-
 }
 
 forgot_password::~forgot_password()
 {
     delete ui;
-}
-
-void forgot_password::click_back_button()
-{
-
-    message = "back";
-
-    emit click_server();
-
-}
-
-void forgot_password::click_forgot_password_button()
-{
-
-    message = "forgot";
-
-    emit click_server();
-
 }
 
 void forgot_password::on_back_button_2_clicked()
@@ -71,190 +33,19 @@ void forgot_password::on_back_button_2_clicked()
 
 void forgot_password::on_forgot_password_button_clicked()
 {
+    QString username = ui->username_lineedit->text();
+    QString password = ui->password_lineedit->text();
+    QString confirm_password = ui->confirm_password_lineedit->text();
 
-    QString username_line;
-
-    username_line = ui->username_lineedit->text();
-
-    bool count = false;
-
-    QFile file_5("files/system_manage_list.txt");
-
-    if(file_5.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-
-        QTextStream in(&file_5);
-
-        QStringList strlist;
-
-        while(!in.atEnd())
-        {
-
-            strlist.append(in.readLine());
-
-        }
-
-        file_5.close();
-
-        for(int i = 0; i < strlist.size() - 1; i += 2)
-        {
-
-            const QString &line_1 = strlist.at(i);
-
-            const QString &line_2 = strlist.at(i+1);
-
-            if(username_line == line_1)
-            {
-
-                count = true;
-
-                ui->password_lineedit->setText(line_2);
-
-            }
-
-        }
-
+    if (username.isEmpty() || password.isEmpty() || confirm_password.isEmpty()) {
+        QMessageBox::warning(this, "Input Error", "Please fill in all fields");
+        return;
     }
-
-    QFile file_6("files/restaurant_managers_list.txt");
-
-    if(file_6.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-
-        QTextStream in(&file_6);
-
-        QStringList strlist;
-
-        while(!in.atEnd())
-        {
-
-            strlist.append(in.readLine());
-
-        }
-
-        file_6.close();
-
-        for(int i = 0; i < strlist.size() - 1; i += 2)
-        {
-
-            const QString &line_1 = strlist.at(i);
-
-            const QString &line_2 = strlist.at(i+1);
-
-            if(username_line == line_1)
-            {
-
-                count = true;
-
-                ui->password_lineedit->setText(line_2);
-
-            }
-
-        }
+    if (password != confirm_password) {
+        QMessageBox::warning(this, "Input Error", "Passwords do not match");
+        return;
     }
-
-    QFile file_7("files/customers_list.txt");
-
-    if(file_7.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-
-        QTextStream in(&file_7);
-
-        QStringList strlist;
-
-        while(!in.atEnd())
-        {
-
-            strlist.append(in.readLine());
-
-        }
-
-        file_7.close();
-
-        for(int i = 0; i < strlist.size() - 1; i += 2)
-        {
-
-            const QString &line_1 = strlist.at(i);
-
-            const QString &line_2 = strlist.at(i+1);
-
-            if(username_line == line_1)
-            {
-
-                count = true;
-
-                ui->password_lineedit->setText(line_2);
-
-            }
-
-        }
-    }
-
-    if(count == 0)
-    {
-
-        QMessageBox::warning(this,"fail","username or password is not correct");
-
-    }
-
-}
-
-void forgot_password::send_message()
-{
-
-    if(socket.state() == QTcpSocket::ConnectedState)
-    {
-
-        QByteArray byte(this->message.c_str(),this->message.length());
-
-        if(socket.write(byte) > 0)
-        {
-
-            if(!socket.waitForBytesWritten(1000))
-            {
-
-                QMessageBox::information(this,"not success","message not send!!!");
-
-            }
-
-        }
-
-    }
-
-}
-
-void forgot_password::receive_message()
-{
-
-    if(socket.state() == QTcpSocket::ConnectedState)
-    {
-
-        if(socket.waitForReadyRead(-1))
-        {
-
-            QByteArray byte = socket.readAll();
-
-            this->message = std::string(byte.constData(),byte.length());
-
-            if(message == "start back")
-            {
-
-                message = "";
-
-                emit click_back();
-
-            }
-            else if(message == "start forgot")
-            {
-
-                message = "";
-
-                emit click_forgot();
-
-            }
-
-        }
-
-    }
-
+    // Simulate password reset
+    QMessageBox::information(this, "Success", "Password has been reset!");
+    on_back_button_2_clicked();
 }
